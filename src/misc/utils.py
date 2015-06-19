dcac7b33e3e443b169ecb89b4c9a226332ef5ece
Andrew Berger <andrew@Andrews-MacBook-Pro.local>
2015-06-19 19:49:49 -0400

    subclassing, override utils

---
+++ src/misc/utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+# external code from stackoverflow: http://stackoverflow.com/questions/1167617/in-python-how-do-i-indicate-im-overriding-a-method
+def overrides(interface_class):
+    """
+    override decorator. need to specify the super class, unfortunately
+    """
+    def overrider(method):
+        assert(method.__name__ in dir(interface_class))
+        return method
+    return overrider
+
+
+def normalize_by_row(matrix):
+    row_sums = matrix.sum(axis=1)
+    # replaces 0 with 1 to avoid divide by 0
+    np.place(row_sums, row_sums == 0, 1)
+    norm_matrix = matrix / row_sums[:, np.newaxis]
+    return norm_matrix
--- src/network/network.py
+++ src/network/network.py
@@ -5,6 +5,8 @@
 # shared variables: weights (input to E, E to I, I to I, I to E),
 # note: following yimengs paper directly, won't need to compute input weights - set by infomax tuning curves with hypercolumns - I worry constrained connectivity will cause problems though
 
+import numpy as np
+from exceptions import NotImplementedError
 
 class Network(object):
     """
@@ -21,3 +23,41 @@ class Network(object):
         self.w_I_E = np.random.randn(n_I, n_E)
         self.theta_E = np.random.randn(n_E)
         self.theta_I = np.random.randn(n_I)
+
+    def update_network(self, input):
+        """
+        Update all nodes according to the relevant update rule
+        input: shape (n_input, )
+        """
+        raise NotImplementedError()
+
+class DiscreteNet(Network):
+    """
+    Superclass for discrete state networks (McCullochPitts and Boltzmann)
+    Contains network representation and share code for asynchronous updates
+    """
+
+    def __init__(self, *args):
+        super(DiscreteNet, self).__init__(*args)
+        # {-1, 1}^n
+        self.y_E = np.ones(n_E)
+        self.y_I = np.ones(n_I)
+
+    def update_unit(input, idx, E):
+        """
+        update the state of the unit at idx
+        E toggles between excitatory and inhibitory neurons
+        """
+        # probably want to switch to some indexing scheme between the arrays
+        raise NotImplementedError
+
+    def update_async(self, input):
+        """
+        Asynchronously update the state of the network given presentation
+         of stimulus input
+        """
+        # potentially override update_net unless there are other intervening steps
+        # maybe run several times for each stimulus
+        async_idx = np.random.permutation(self.n_E + self.n_I)
+        for idx in async_idx:
+            self.update_unit(input, idx, idx < self.n_E)

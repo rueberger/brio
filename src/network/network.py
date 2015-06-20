3fcaf9f799e4b8dd7328e31431b94569d4c584f3
Andrew Berger <andrew@Andrews-MacBook-Pro.local>
2015-06-19 21:20:47 -0400

    pylint chaos

--- src/network/network.py
+++ src/network/network.py
@@ -3,7 +3,8 @@
 # shared hidden methods: update state (asynchronous for now, which rules out the LIF neurons used in King et al - a major question to be answered is why they chose that scheme and if it will,
 # work with boltzmann machines)
 # shared variables: weights (input to E, E to I, I to I, I to E),
-# note: following yimengs paper directly, won't need to compute input weights - set by infomax tuning curves with hypercolumns - I worry constrained connectivity will cause problems though
+# to do: implement inhibitory pooling in the input
+
 
 import numpy as np
 from exceptions import NotImplementedError
@@ -37,13 +38,34 @@ class DiscreteNet(Network):
     Contains network representation and share code for asynchronous updates
     """
 
+    # async is really inefficient
+    # can get away with sync updates?
+
     def __init__(self, *args):
         super(DiscreteNet, self).__init__(*args)
         # {-1, 1}^n
         self.y_E = np.ones(n_E)
         self.y_I = np.ones(n_I)
 
-    def update_unit(input, idx, E):
+    def unit_energy(self, input, idx, E):
+        """
+        Returns the energy of the unit at idx
+        E toggles excited or inhibitory
+        """
+        if E:
+            E_input = np.sum(self.w_inp_E[:, idx] * input)
+            E_inhib = -np.sum(self.w_I_E[:, idx] * self.y_I)
+            # sign error here?
+            E_bias = self.theta_E[idx] * self.y_E[idx]
+            return E_inhib + E_input + E_bias
+        else:
+            E_excit = np.sum(self.w_E_I[:, idx] * self.y_E[idx])
+            E_inhib = np.sum(self.w_I_I[:, idx] * self.y_I[idx])
+            E_bias = np.sum(self.theta_I[idx] * self.y_I[idx])
+            return E_inhib + E_excit + E_bias
+
+
+    def update_unit(self, input, idx, E):
         """
         update the state of the unit at idx
         E toggles between excitatory and inhibitory neurons
@@ -61,3 +83,13 @@ class DiscreteNet(Network):
         async_idx = np.random.permutation(self.n_E + self.n_I)
         for idx in async_idx:
             self.update_unit(input, idx, idx < self.n_E)
+
+class BoltzmannMachine(DiscreteNet):
+    """
+    Boltzmann Machine
+    """
+
+    @overrides(DiscreteNet)
+    def update_unit(input, idx, E):
+        if E:
+            pass

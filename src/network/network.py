c6114263e231780bbc801d14935c3a61cc77a42e
Andrew Berger <andrew@Andrews-MacBook-Pro.local>
2015-06-22 14:33:21 -0400

    pylint fixes

--- src/network/network.py
+++ src/network/network.py
@@ -7,7 +7,6 @@
 
 
 import numpy as np
-from exceptions import NotImplementedError
 
 class Network(object):
     """
@@ -18,17 +17,20 @@ class Network(object):
     """
 
     def __init__(self, n_input, n_E, n_I):
-        self.w_inp_E = np.random.randn(n_input, n_E)
-        self.w_E_I = np.random.randn(n_E, n_I)
-        self.w_I_I = np.random.randn(n_I, n_I)
-        self.w_I_E = np.random.randn(n_I, n_E)
-        self.theta_E = np.random.randn(n_E)
-        self.theta_I = np.random.randn(n_I)
+        self.n_in = n_input
+        self.n_i = n_I
+        self.n_e = n_E
+        self.w_inp_e = np.random.randn(n_input, n_E)
+        self.w_e_i = np.random.randn(n_E, n_I)
+        self.w_i_i = np.random.randn(n_I, n_I)
+        self.w_i_e = np.random.randn(n_I, n_E)
+        self.theta_e = np.random.randn(n_E)
+        self.theta_i = np.random.randn(n_I)
 
-    def update_network(self, input):
+    def update_network(self, in):
         """
         Update all nodes according to the relevant update rule
-        input: shape (n_input, )
+        in: shape (n_input, )
         """
         raise NotImplementedError()
 
@@ -44,28 +46,29 @@ class DiscreteNet(Network):
     def __init__(self, *args):
         super(DiscreteNet, self).__init__(*args)
         # {-1, 1}^n
-        self.y_E = np.ones(n_E)
-        self.y_I = np.ones(n_I)
+        self.y_e = np.ones(self.n_e)
+        self.y_i = np.ones(self.n_i)
 
-    def unit_energy(self, input, idx, E):
+    def unit_energy(self, input, idx, excitatory):
         """
         Returns the energy of the unit at idx
         E toggles excited or inhibitory
         """
-        if E:
-            E_input = np.sum(self.w_inp_E[:, idx] * input)
-            E_inhib = -np.sum(self.w_I_E[:, idx] * self.y_I)
+        if excitatory:
+            e_input = np.sum(self.w_inp_e[:, idx] * input)
+            e_inhib = -np.sum(self.w_i_e[:, idx] * self.y_i)
+
             # sign error here?
-            E_bias = self.theta_E[idx] * self.y_E[idx]
-            return E_inhib + E_input + E_bias
+            e_bias = self.theta_e[idx] * self.y_e[idx]
+            return e_inhib + e_input + e_bias
         else:
-            E_excit = np.sum(self.w_E_I[:, idx] * self.y_E[idx])
-            E_inhib = np.sum(self.w_I_I[:, idx] * self.y_I[idx])
-            E_bias = np.sum(self.theta_I[idx] * self.y_I[idx])
-            return E_inhib + E_excit + E_bias
+            e_excit = np.sum(self.w_e_i[:, idx] * self.y_e[idx])
+            e_inhib = np.sum(self.w_i_i[:, idx] * self.y_i[idx])
+            e_bias = np.sum(self.theta_i[idx] * self.y_i[idx])
+            return e_inhib + e_excit + e_bias
 
 
-    def update_unit(self, input, idx, E):
+    def update_unit(self, input, idx, excitatory):
         """
         update the state of the unit at idx
         E toggles between excitatory and inhibitory neurons
@@ -80,9 +83,9 @@ class DiscreteNet(Network):
         """
         # potentially override update_net unless there are other intervening steps
         # maybe run several times for each stimulus
-        async_idx = np.random.permutation(self.n_E + self.n_I)
+        async_idx = np.random.permutation(self.n_e + self.n_i)
         for idx in async_idx:
-            self.update_unit(input, idx, idx < self.n_E)
+            self.update_unit(input, idx, idx < self.n_e)
 
 class BoltzmannMachine(DiscreteNet):
     """

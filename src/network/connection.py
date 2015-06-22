71e98bdd5b740fed77cb7007c2a88153f7eafaee
Andrew Berger <andrew@Andrews-MBP.wv.cc.cmu.edu>
2015-06-22 18:26:01 -0400

    basic architecture wip

--- src/network/connection.py
+++ src/network/connection.py
@@ -7,22 +7,41 @@ class Connection(object):
     holds network weights
     """
 
-    def __init__(self, input_layer, output_layer):
+    def __init__(self, input_layer, output_layer, constraint=None):
         # to do: add excitatory flag
         self.input_layer = input_layer
         self.output_layer = output_layer
         # allow for specification of input method
         self.weights = np.random.randn(input_layer.ndims, output_layer.ndims)
         self.output_layer.add_input(self)
+        self.input_layer.add_output(self)
+        if constraint is not None:
+            self.constraint_multiplier = {
+                'excitatory': 1,
+                'inhibitory': -1
+            }[constraint]
 
 
-    def get_energy(self, idx):
+    # input energy and output energy methods needed
+
+    def feedforward_energy(self, idx):
         """
         Returns the output into the unit at idx of the output layer
         """
         return self.weights[:, idx] * self.input_layer.state
 
+    # to do: better name needed
+    def energy_shadow(self, input_idx):
+        """
+        return the energy of the output states 'shadowed' by the input unit
+          at input_idx
+        for use in calculating energy difference for boltzmann machines
+        """
+        return self.weights[input_idx, :] * self.output_layer.state
 
-
-
-# subclass: constrainedconnection
+    def __impose_constraint(self):
+        """
+        Constrain the weights according to the constraint multiplier
+        """
+        out_of_bounds_idx = (self.weights * self.constraint_multiplier < 0)
+        self.weights[out_of_bounds_idx] = 0
--- src/network/layer.py
+++ src/network/layer.py
@@ -1,4 +1,5 @@
 
+from misc.utils import overrides
 import numpy as np
 
 class Layer(object):
@@ -11,8 +12,10 @@ class Layer(object):
         self.n_dims = n_dims
         self.state = np.ones(n_dims)
         # to do allow for specification of init method
+        # implement mean histories
         self.bias = np.random.randn(n_dims)
         self.inputs = []
+        self.outputs = []
 
     def activation(self, energy):
         """
@@ -27,17 +30,74 @@ class Layer(object):
         add input_connection to the list of connections feeding into this layer
         """
         self.inputs.append(input_connection)
-        # assert the inputs have the right dimension
+
+    def add_output(self, output_connection):
+        """
+        add output_connection to the list of connections feeding out of this layer
+        """
+        self.outputs.append(output_connection)
+
+    def input_energy(self, idx):
+        """
+        returns the energy fed into the the unit at idx by all input layers
+        for feedforward networks this is the only relevant energy method
+        """
+        energy = 0
+        for input_layer in self.inputs:
+            energy += input_layer.feedforward_energy(idx)
+        return energy
+
+    def output_energy(self, idx):
+        """
+        returns the energy this unit feeds into its output layers
+        for use in calculating the energy difference of a bitflip for boltzmann machines
+        """
+        energy = 0
+        for output_layer in self.outputs:
+            energy += output_layer.energy_shadow(idx)
 
     def update(self, idx):
         """
         update the state at idx
         """
-        energy = self.bias[idx]
-        for input_layer in self.inputs:
-            energy += input_layer.get_energy(idx)
+        raise NotImplementedError
+
+
+class BoltzmannMachineLayer(Layer):
+    """
+    Implements the Boltzman Machine actiation function
+    """
+
+    @overrides(Layer)
+    def activation(self, energy):
+        # might want an extra factor of two here to account
+        # for the energy difference
+        p_on = 1. / (1 + np.exp(-energy))
+        if np.random.random() < p_on:
+            return 1
+        else:
+            return -1
+
+    @overrides(Layer)
+    def update(self, idx):
+        e_diff = self.bias[idx]
+        e_diff += self.input_energy(idx)
+        e_diff += self.output_energy(idx)
+        self.state[idx] = self.activation(e_diff)
+
+class PerceptronLayer(Layer):
+    """
+    Simple feedforward perceptron with a hard threshold activation function
+    """
+
+    @overrides(Layer)
+    def activation(self, energy):
+        if energy > 0:
+            return 1
+        else:
+            return -1
+
+    @overrides(Layer)
+    def update(self, idx):
+        energy = self.bias[idx] + self.input_energy(idx)
         self.state[idx] = self.activation(energy)
-
-
-
-# subclassing for input layer may not be necessary

7c33d476d2bb02e0425a695ccdc49c206b8302ce
Andrew Berger <andrew@Andrews-MBP.wv.cc.cmu.edu>
2015-06-23 15:18:02 -0400

    playing with pythoscope

---
+++ src/.pythoscope/points-of-entry/rbm_factory.py
@@ -0,0 +1,5 @@
+"""
+point of entry for pythoscope auto test generation
+"""
+from blocks.factories import rbm_factory
+rbm = rbm_factory([5, 10, 50, 10, 5])
---
+++ src/tests/test_network_layer.py
@@ -0,0 +1,67 @@
+import unittest
+
+class TestLayer(unittest.TestCase):
+    def test___init__(self):
+        # layer = Layer(n_dims)
+        assert False # TODO: implement your test here
+
+    def test_activation(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.activation(energy))
+        assert False # TODO: implement your test here
+
+    def test_add_input(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.add_input(input_connection))
+        assert False # TODO: implement your test here
+
+    def test_add_output(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.add_output(output_connection))
+        assert False # TODO: implement your test here
+
+    def test_input_energy(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.input_energy(idx))
+        assert False # TODO: implement your test here
+
+    def test_output_energy(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.output_energy(idx))
+        assert False # TODO: implement your test here
+
+    def test_update(self):
+        # layer = Layer(n_dims)
+        # self.assertEqual(expected, layer.update(idx))
+        assert False # TODO: implement your test here
+
+class TestBoltzmannMachineLayer(unittest.TestCase):
+    def test_activation(self):
+        # boltzmann_machine_layer = BoltzmannMachineLayer()
+        # self.assertEqual(expected, boltzmann_machine_layer.activation(energy))
+        assert False # TODO: implement your test here
+
+    def test_update(self):
+        # boltzmann_machine_layer = BoltzmannMachineLayer()
+        # self.assertEqual(expected, boltzmann_machine_layer.update(idx))
+        assert False # TODO: implement your test here
+
+class TestPerceptronLayer(unittest.TestCase):
+    def test_activation(self):
+        # perceptron_layer = PerceptronLayer()
+        # self.assertEqual(expected, perceptron_layer.activation(energy))
+        assert False # TODO: implement your test here
+
+    def test_update(self):
+        # perceptron_layer = PerceptronLayer()
+        # self.assertEqual(expected, perceptron_layer.update(idx))
+        assert False # TODO: implement your test here
+
+class TestInputLayer(unittest.TestCase):
+    def test_set_state(self):
+        # input_layer = InputLayer()
+        # self.assertEqual(expected, input_layer.set_state(state))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()

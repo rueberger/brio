a2f2b219d6db90e2cd4c18ce6d9745b0ec7c3486
Andrew Berger <andbberger@berkeley.edu>
2015-06-25 02:13:51 -0400

    added some tests for layer

--- src/tests/test_blocks_layer.py
+++ src/tests/test_blocks_layer.py
@@ -1,56 +1,25 @@
+"""
+unit test for blocks.layer
+"""
 import numpy as np
 import unittest
-from blocks.layers import Layer, BoltzmanMachineLayer, PerceptronLayer, InputLayer
+from blocks.layer import Layer, BoltzmanMachineLayer, PerceptronLayer, InputLayer
 
 N_SEED = 1337
 N_DIMS = 10
 
 class TestLayer(unittest.TestCase):
 
-    def test_add_input(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.add_input(input_connection))
-        assert False # TODO: implement your test here
-
-    def test_add_output(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.add_output(output_connection))
-        assert False # TODO: implement your test here
-
-    def test_apply_bias_rule(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.apply_bias_rule())
-        assert False # TODO: implement your test here
-
-    def test_firing_rates(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.firing_rates())
-        assert False # TODO: implement your test here
-
-    def test_input_energy(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.input_energy(idx))
-        assert False # TODO: implement your test here
-
-    def test_output_energy(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.output_energy(idx))
-        assert False # TODO: implement your test here
-
-    def test_unpack_network_params(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.unpack_network_params(network))
-        assert False # TODO: implement your test here
+    def setUp(self):
+        np.random.seed(N_SEED)
+        self.test_layer = Layer(N_DIMS)
 
     def test_update_history(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.update_history())
-        assert False # TODO: implement your test here
+        self.test_layer.max_history_length = 5
+        self.test_layer.state = 5 * np.ones(N_DIMS)
+        self.test_layer.update_history()
+        self.test_layer.state = np.ones(N_DIMS)
 
-    def test_update_state(self):
-        # layer = Layer(N_DIMS, ltype)
-        # self.assertEqual(expected, layer.update_state(idx))
-        assert False # TODO: implement your test here
 
 class TestBoltzmannMachineLayer(unittest.TestCase):
 
@@ -59,7 +28,14 @@ class TestBoltzmannMachineLayer(unittest.TestCase):
         self.test_layer = BoltzmanMachineLayer(N_DIMS)
 
     def test_activation(self):
+        # test a particular value
         self.assertEqual(1, self.test_layer.activation(np.inf))
+        # test validity of returned values
+        allowed_updates = {1, -1}
+        for energy in 5 * np.random.randn(250):
+            self.assertTrue(self.test_layer.activation(energy) in allowed_updates)
+
+
 
 class TestPerceptronLayer(unittest.TestCase):
 
@@ -68,7 +44,14 @@ class TestPerceptronLayer(unittest.TestCase):
         self.test_layer = PerceptronLayer(N_DIMS)
 
     def test_activation(self):
+        # test a particular value
         self.assertEqual(1, self.test_layer.activation(np.inf))
+        # test validity of returned values
+        allowed_updates = {1, -1}
+        for energy in 5 * np.random.randn(250):
+            self.assertTrue(self.test_layer.activation(energy) in allowed_updates)
+
+
 
 class TestInputLayer(unittest.TestCase):
 

60b470011ddb7912c8b0bf5ac6c91f55cf97234d
Andrew Berger <andbberger@berkeley.edu>
2015-06-24 23:57:17 -0400

    pythoscope generated test files

---
+++ src/tests/test_blocks_network.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestNetwork(unittest.TestCase):
+    def test___init__(self):
+        # network = Network(layers, params)
+        assert False # TODO: implement your test here
+
+    def test_training_iteration(self):
+        # network = Network(layers, params)
+        # self.assertEqual(expected, network.training_iteration())
+        assert False # TODO: implement your test here
+
+    def test_update_network(self):
+        # network = Network(layers, params)
+        # self.assertEqual(expected, network.update_network(stimulus))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()

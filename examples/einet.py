61d6f4b32b22324987a11c7fe32eb25bd291fea2
Andrew Berger <andberger@berkeley.edu>
2015-06-29 12:43:06 -0400

    added example

--- examples/einet.py
+++ examples/einet.py
@@ -13,14 +13,26 @@ from scipy.io import loadmat
 DATA_PATH = "../data/whitened_van_hateren.mat"
 PATCH_SIZE = 16
 
-# creates an EI-net as described in Deweese and King 2013
-# input layer with 256 ndoes (same as image patches)
-# excitatory layer with 256 nodes
-# inhibitory layer with 49 nodes
-einet = einet_factory([PATCH_SIZE ** 2, 256, 49])
+images = loadmat(DATA_PATH)['IMAGES']
 
-# trains the network
-for patch in patch_generator:
-    einet.run_network(patch)
 
-# that's it!
+def run_example():
+    """
+    Creates an EI-net as described in Deweese and King 2013
+    trains it on whitened image patches from the van hateren database
+    demonstrates the learned receptive fields
+    """
+    # input layer with 256 ndoes (same as image patches)
+    # excitatory layer with 256 nodes
+    # inhibitory layer with 49 nodes
+    einet = einet_factory([PATCH_SIZE ** 2, 256, 49])
+
+    # this method iterates through the stimuli
+    # presents each stimulus to the network a number of times (5 by default)
+    #  and updates the state asynchronously
+    # next, weights and biases are updated according to the learning rules defined in
+    #  their class
+    einet.train(patch_generator(images, PATCH_SIZE, 5000))
+
+    # that's it!
+    return einet

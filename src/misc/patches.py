b74fd74b45d7681356a623bf1219c85097e5d704
Andrew Berger <andberger@berkeley.edu>
2015-07-06 12:22:05 -0400

    normalized patches....

--- src/misc/patches.py
+++ src/misc/patches.py
@@ -3,7 +3,7 @@ Utils for generating and processing patches from images
 """
 import numpy as np
 
-def patch_generator(images, patch_size, n_patches=1000, crop=4):
+def patch_generator(images, patch_size, n_patches=1000, crop=4, normalize=True):
     """ A generator that produces patches of the desired size from images
 
     :param images: an array of images. expected shape (img_height, img_width,  n_imgs)
@@ -22,4 +22,10 @@ def patch_generator(images, patch_size, n_patches=1000, crop=4):
         hgt_idx = np.random.randint(height_max_draw)  + crop
         wdt_idx = np.random.randint(width_max_draw) + crop
         img_idx = np.random.randint(n_imgs)
-        yield images[hgt_idx : hgt_idx + patch_size, wdt_idx : wdt_idx + patch_size, img_idx]
+        img = images[hgt_idx : hgt_idx + patch_size, wdt_idx : wdt_idx + patch_size, img_idx]
+        if normalize:
+            img -= np.mean(img)
+            img *= 1. / np.std(img)
+            yield img
+        else:
+            yield img

carefully corrected for units... performance is much better now

examples/einet.py
@@ -6,8 +6,8 @@ Whitened images used here available from Bruno Olshausen at the
   redwood center here: https://redwood.berkeley.edu/bruno/sparsenet/
 """
 
-from blocks.factories import einet_factory
-from misc.patches import patch_generator
+from blocks.factories import einet_factory, sailnet_factory, perceptron_factory
+from misc.patches import patch_generator, mean_zero_patch
 from scipy.io import loadmat
 from blocks.aux import NetworkParams
 from misc.plotting import plot_receptive_fields, plot_param_distr
@@ -36,13 +36,25 @@ def run_example():
     #  and updates the state asynchronously
     # next, weights and biases are updated according to the learning rules defined in
     #  their class
-    einet.train(patch_generator(images, PATCH_SIZE, 1000))
+    einet.train(patch_generator(images, PATCH_SIZE, 5000))
+    # that's it
+    plt.clf()
 
-    plot_param_distr(einet)
-
-    print "Now plotting receptive fields"
     plot_receptive_fields(einet, 1, slideshow=False, n_samples=1000,
-                          stimulus_generator=patch_generator(images, PATCH_SIZE, int(1E5)))
 
-    # that's it!
+
+                          stimulus_generator=patch_generator(images, PATCH_SIZE, int(1E5)))
     return einet
+
+def sailnet():
+    snet = sailnet_factory([PATCH_SIZE **2, 256], NetworkParams(async=False, display=True))
+    snet.train(patch_generator(images, PATCH_SIZE, 10000))
+    return snet
+
+def oja():
+    onet = perceptron_factory([PATCH_SIZE **2, 64], NetworkParams(async=False, display=False))
+    onet.train(mean_zero_patch(images, PATCH_SIZE, 5000))
+    plot_receptive_fields(onet, 1, slideshow=False, n_samples=1000,
+                          stimulus_generator=mean_zero_patch(images, PATCH_SIZE, int(1E5)))
+
+    return onet

src/blocks/connection.py
@@ -87,7 +87,7 @@ class Connection(object):
         :rtype: None
         """
         self.params = network.params
-        self.learning_rate = self.lrate_multiplier *  network.params.baseline_lrate
+        self.learning_rate = self.lrate_multiplier *  network.params.baseline_lrate / self.params.timestep
         self.epoch_size = self.params.update_batch_size
 
     def __impose_constraint(self):
@@ -146,7 +146,7 @@ class FoldiakConnection(Connection):
         post_syn_avg_rates = self.postsynaptic_layer.lfr_mean
         delta = (np.dot(pre_syn_rates.T, post_syn_rates) -
                  np.outer(pre_syn_avg_rates, post_syn_avg_rates) * self.epoch_size)
-        self.weights += self.learning_rate * delta
+        self.weights += self.learning_rate * delta / self.epoch_size
 
 
 
@@ -162,13 +162,13 @@ class CMConnection(Connection):
     def bulk_weight_update(self):
         pre_syn_rates = np.array(self.presynaptic_layer.fr_history[:self.epoch_size])
         post_syn_rates = np.array(self.postsynaptic_layer.fr_history[:self.epoch_size])
-        # pre_syn_avg_rates = self.presynaptic_layer.lfr_mean
-        # post_syn_avg_rates = self.postsynaptic_layer.lfr_mean
-        pre_syn_avg_rates = self.presynaptic_layer.target_firing_rate
-        post_syn_avg_rates = self.postsynaptic_layer.target_firing_rate
+        pre_syn_avg_rates = self.presynaptic_layer.lfr_mean * self.params.timestep
+        post_syn_avg_rates = self.postsynaptic_layer.lfr_mean * self.params.timestep
+        # pre_syn_avg_rates = self.presynaptic_layer.target_firing_rate
+        # post_syn_avg_rates = self.postsynaptic_layer.target_firing_rate
         delta = (np.dot(pre_syn_rates.T, post_syn_rates) -
-                 np.outer(pre_syn_avg_rates, post_syn_avg_rates) *
-                 self.epoch_size * (1 + self.weights))
+                 np.outer(pre_syn_avg_rates, post_syn_avg_rates)
+                 * (1 + self.weights) * self.epoch_size)
         self.weights += self.learning_rate * delta
 
 class ConstantConnection(Connection):

src/blocks/layer.py
@@ -87,10 +87,10 @@ class Layer(object):
         # inelegant and I hope not necessary but this comes directly out of the
         # EI net implementation
         if self.update_bias:
-            non_windowed_rate = np.mean(self.history[:self.max_history_length], axis=0)
-            delta = self.target_firing_rate - non_windowed_rate
-            #delta = self.target_firing_rate - self.lfr_mean
-            self.bias += (self.update_sign * self.learning_rate * self.params.update_batch_size * delta)
+            epoch_time_units = self.params.update_batch_size * self.params.timestep
+            delta = self.target_firing_rate - self.epoch_fr
+            self.bias += (self.update_sign * self.learning_rate * delta * epoch_time_units)
+            # sensible for LIF neurons but will want to change for others....
             self.bias[self.bias < 0] = 0
 
     def add_input(self, input_connection):
@@ -167,8 +167,10 @@ class Layer(object):
         :returns: None
         :rtype: None
         """
-        epoch_mean = np.mean(self.fr_history[:self.params.layer_history_length], axis=0)
-        self.lfr_mean += self.params.ema_lfr * (epoch_mean - self.lfr_mean)
+        self.epoch_fr = (np.mean(self.fr_history[:self.params.layer_history_length], axis=0) /
+                         self.params.timestep)
+        self.lfr_mean += self.params.ema_lfr * (np.mean(self.firing_rates, axis=0) / self.params.timestep
+                                                - self.lfr_mean)
 
 
 
@@ -261,8 +263,8 @@ class LIFLayer(Layer):
     @overrides(Layer)
     def reset(self):
         self.state = np.zeros(self.n_dims)
-        self.potentials = np.random.random(self.n_dims) * self.bias
-        #self.potentials = np.zeros(self.n_dims)
+#        self.potentials = np.random.random(self.n_dims) * self.bias
+        self.potentials = np.zeros(self.n_dims)
 
 
 class BoltzmannMachineLayer(Layer):

src/blocks/network.py
@@ -69,7 +69,9 @@ class Network(object):
         :rtype: None
         """
         print "Training iteration: {}".format(self.t_counter)
-        print "Example firing rate: {}".format(self.layers[1].firing_rates[0])
+        print "Firing rates:"
+        for layer in self.layers[1:]:
+            print "{}: {}".format(str(layer), np.mean(layer.epoch_fr))
         if self.params.display:
             self.param_plot.update_plot()
 
@@ -87,8 +89,7 @@ class Network(object):
             self.t_counter += 1
             if idx % self.params.stimuli_per_epoch == 0 and idx != 0:
                 self.training_iteration()
-                if idx % 1000 == 0:
-                    self.describe_progress()
+                self.describe_progress()
 
 
     def update_network(self, stimulus):

src/misc/patches.py
@@ -26,9 +26,10 @@ def patch_generator(images, patch_size, n_patches=1000, crop=4, normalize=True):
         if normalize:
             img -= np.mean(img)
             img *= 1. / np.std(img)
-            yield img
+            # divide by five as prescribed in the EINet paper
+            yield img / 5.
         else:
-            yield img
+            yield img / 5.
 
 def mean_zero_patch(images, patch_size, n_patches, crop=4):
     patches = np.array(list(patch_generator(images, patch_size, n_patches, crop, True)))
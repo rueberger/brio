fixed bias shape changed lifetime mean to be the ewa of standard epoch mean need to be careful about units for that

src/blocks/layer.py
@@ -34,7 +34,7 @@ class Layer(object):
         """
 
         self.n_dims = n_dims
-        self.bias = np.ones(self.n_dims) * 2
+        self.bias = np.ones((self.n_dims, 1)
         self.inputs = []
         self.outputs = []
         self.ltype = ltype
@@ -160,12 +160,8 @@ class Layer(object):
         :rtype: None
         """
         self.epoch_fr = (
-            np.mean(self.fr_history[:self.params.layer_history_length], axis=(0, 2)) /
-            self.params.timestep
-        )
-        self.lfr_mean += (self.params.ema_lfr *
-                          ((np.mean(self.firing_rates, axis=(0, 2)) /
-                            self.params.timestep) - self.lfr_meany))
+            np.mean(self.fr_history[:self.params.layer_history_length], axis=(0, 2)) / self.params.timestep)
+        self.lfr_mean += (self.params.ema_lfr * self.epoch_fr / self.params.timestep) - self.lfr_mean))
 
 
     def update_history(self):
@@ -189,9 +185,10 @@ class Layer(object):
         :returns: None
         :rtype: None
         """
-        self.fr_history = []
-        self.history = []
         self.reset_state_vars()
+        self.fr_history = []
+        self.history = [self.state.copy()]
+
 
     def reset_state_vars(self):
         """ Reset the state variables for this layer such as state
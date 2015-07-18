40d2a2fe92e869f8653a08a18dda911bd4f759e0
Andrew Berger <andberger@berkeley.edu>
2015-07-17 23:02:41 -0400

    gated einet factory

--- src/blocks/factories.py
+++ src/blocks/factories.py
@@ -86,3 +86,35 @@ def perceptron_factory(layer_sizes, params=NetworkParams()):
     layers[1].bias = np.zeros(layer_sizes[1])
     connection.OjaConnection(layers[0], layers[1])
     return network.Network(layers, params)
+
+def gated_einet_factory(layer_sizes, n_input_stimuli, params=NetworkParams()):
+    """ Constructs an EInet with multiplicatively gated input
+    That is, the excitatory current receives current:
+      I_in = W_1 x_1 \cdot W_2 x_2 \dots W_{n-1} x_{n-1} \cdot W_n x_n
+    where x_i is the ith input stimuli, W_i is the ith set of weights,
+      n = n_input_stimuli and \cdot indicates a dot product
+
+    :param layer_sizes: list of the layer sizes. Must be of length 3.
+      format is [input_size, excitatory_size, inhibitory_size]
+      in general the inhibitory layer should be much smaller than the excitatory layer
+    :param n_input_stimuli: the number of input stimuli that will be gated multiplicatively
+    :param params: a NetworkParams object with global parameters for this network
+    :returns: the gated EInet
+    :rtype: Network
+    """
+    assert len(layer_sizes) == 3
+    # might as well use standard EINet
+    assert n_input_stimuli > 1
+    layers = [
+        layer.SplitInput(layer_sizes[0], n_input_stimuli),
+        layer.LIFLayer(layer_sizes[1], LayerType.excitatory),
+        layer.LIFLayer(layer_sizes[2], LayerType.inhibitory, allow_self_con=False)
+    ]
+	for child_layer in layers[0].children:
+        layers.append(child_layer)
+        input_con = connection.OjaConnection(child_layer, layers[1], lrate_multiplier=0.1)
+        input_con.weight_multiplier = 5
+    connection.CMConnection(layers[1], layers[2], weight_scheme='uniform', lrate_multiplier=.7)
+    connection.CMConnection(layers[2], layers[2],weight_scheme='zero', lrate_multiplier=1.5)
+    connection.CMConnection(layers[2], layers[1], weight_scheme='uniform', lrate_multiplier=0.7)
+    return network.Network(layers, params)

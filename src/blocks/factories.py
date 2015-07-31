92c61343a98346187a31078916a8eace25fb260b
Andrew Berger <andberger@berkeley.edu>
2015-07-31 12:37:29 -0400

    removed some less than useful factories

--- src/blocks/factories.py
+++ src/blocks/factories.py
@@ -19,21 +19,6 @@ def rbm_factory(layer_sizes):
         connection.CMConnection(input_layer, output_layer)
     return network.Network(layers)
 
-def mlp_factory(layer_sizes):
-    """ Constructs a Multilayer Perceptron network
-
-    :param layer_sizes: list of the size of each layer. Input layer size first element, output last
-    :returns: the constructed MLP net
-    :rtype: Network
-    """
-    layers = []
-    layers.append(layer.InputLayer(layer_sizes[0]))
-    for lsize in layer_sizes[1:]:
-        layers.append(layer.PerceptronLayer(lsize))
-    for input_layer, output_layer in zip(layers[:-1], layers[1:]):
-        connection.CMConnection(input_layer, output_layer)
-    return network.Network(layers)
-
 def einet_factory(layer_sizes, params=NetworkParams()):
     """ Constructs EI-net, inspired from King and Deweese 2013 (with some differences)
     Use Boltzmann machine units for now
@@ -74,17 +59,6 @@ def sailnet_factory(layer_sizes, params=NetworkParams()):
     ]
     connection.OjaConnection(layers[0], layers[1], lrate_multiplier=0.2)
     connection.FoldiakConnection(layers[1], layers[1], weight_scheme='uniform', lrate_multiplier=.7)
-    return network.Network(layers, params)
-
-def perceptron_factory(layer_sizes, params=NetworkParams()):
-    import numpy as np
-    assert len(layer_sizes) == 2
-    layers = [
-        layer.InputLayer(layer_sizes[0]),
-        layer.PerceptronLayer(layer_sizes[0], update_bias=False)
-    ]
-    layers[1].bias = np.zeros(layer_sizes[1])
-    connection.OjaConnection(layers[0], layers[1])
     return network.Network(layers, params)
 
 def gated_einet_factory(layer_sizes, n_input_stimuli, params=NetworkParams()):

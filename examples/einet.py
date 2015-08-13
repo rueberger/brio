f38d518b7d288a9c4deb407faf686c071ce45a8d
Andrew Berger <andberger@berkeley.edu>
2015-08-13 16:55:16 -0700

    lingering changes... small tweaks

--- examples/einet.py
+++ examples/einet.py
@@ -6,7 +6,7 @@ Whitened images used here available from Bruno Olshausen at the
   redwood center here: https://redwood.berkeley.edu/bruno/sparsenet/
 """
 
-from blocks.factories import einet_factory, sailnet_factory, perceptron_factory
+from blocks.factories import einet_factory, sailnet_factory
 from misc.patches import patch_generator, mean_zero_patch
 from scipy.io import loadmat
 from blocks.aux import NetworkParams
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 
 DATA_PATH = "../data/whitened_van_hateren.mat"
-PATCH_SIZE = 10
+PATCH_SIZE = 17
 
 images = loadmat(DATA_PATH)['IMAGES']
 
@@ -27,16 +27,16 @@ def run_example():
     demonstrates the learned receptive fields
     """
     params = NetworkParams(async=False, display=False)
-    params.update_cap = 0.25
+    params.update_cap = 0.1
     params.steps_per_rc_time = 10
     params.steps_per_fr_time = 10
     params.bias_learning_rate = 0.2
-    params.baseline_lrate = 0.05
+    params.baseline_lrate = 0.02
     params.baseline_firing_rate = 0.02
     params.lfr_char_time = 5
-    einet = einet_factory([PATCH_SIZE ** 2, 400, 49], params)
+    einet = einet_factory([PATCH_SIZE ** 2, 625, 64], params)
     fig, ax = plt.subplots(figsize=(10,10))
-    for _ in xrange(20):
+    for _ in xrange(40):
         einet.train(patch_generator(images, PATCH_SIZE, 1000))
         oja_w = einet.layers[0].outputs[0].weights.T
         imgs = [w.reshape(PATCH_SIZE, PATCH_SIZE) for w in oja_w]
@@ -61,11 +61,3 @@ def sailnet():
         plot_concat_imgs(imgs, axis=ax)
         plt.draw()
     return snet
-
-def oja():
-    onet = perceptron_factory([PATCH_SIZE **2, 64], NetworkParams(async=False, display=False))
-    onet.train(mean_zero_patch(images, PATCH_SIZE, 5000))
-    plot_receptive_fields(onet, 1, slideshow=False, n_samples=1000,
-                          stimulus_generator=mean_zero_patch(images, PATCH_SIZE, int(1E5)))
-
-    return onet
